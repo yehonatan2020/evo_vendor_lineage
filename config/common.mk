LeafOS-Project/android_vendor_leaf

vendor: Introduce App Lock [4/4]
Change-Id: Ie6d86e301efe14146c961b11bfccd3fa74a1562c

config/common.mk
@@ -78,6 +78,10 @@ PRODUCT_PACKAGE_OVERLAYS += \
     vendor/leaf/overlay/common \
     vendor/leaf/overlay/no-rro
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    vendor/leaf/config/permissions/privapp-permissions-settings.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-settings.xml
+
 # PDF
 PRODUCT_PACKAGES += \
     MuPDF

yehonatan2020/evo_vendor_lineage

config/common.mk
@@ -196,6 +196,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/etc/init/init.openssh.rc:$(TARGET_COPY_OUT_PRODUCT)/etc/init/init.openssh.rc
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    vendor/lineage/config/permissions/privapp-permissions-settings.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-settings.xml
+
 # rsync
 PRODUCT_PACKAGES += \
     rsync